87a89559deb4b00fe00f0a66b1ac455e0657ff60
Raman Deep <deep.raman85@gmail.com>
2018-06-14 09:50:54 +0200

    Added timeout and max tries to 2 to prevent the script to check same domain forever

--- check_redirect.sh
+++ check_redirect.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE="redirection_output.txt"
 DOMAINS=$(cat $DOMAINS_FILE)
 for DOMAIN in $DOMAINS; do
 	#echo "$DOMAIN"
-	REDIRECTION=$(wget $DOMAIN 2>&1 | grep Location: | tail -n 1 | cut -d ":" -f3 | sed 's/\[following\]//g' | sed 's/\///g')
+	REDIRECTION=$(wget --timeout=5 --tries=2 $DOMAIN 2>&1 | grep Location: | tail -n 1 | cut -d ":" -f3 | sed 's/\[following\]//g' | sed 's/\///g')
 	if [[ -z "$REDIRECTION" ]]; then
 		echo "$DOMAIN ==> KO" >> $OUTPUT_FILE
 	else
