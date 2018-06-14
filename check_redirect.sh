70d6a91dc3c5c888c628e2f3da839c8d1a6cb443
Raman Deep <deep.raman85@gmail.com>
2018-06-14 09:41:05 +0200

    Added script to check domain redirection

---
+++ check_redirect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOMAINS_FILE="domains.txt"
+OUTPUT_FILE="redirection_output.txt"
+DOMAINS=$(cat $DOMAINS_FILE)
+for DOMAIN in $DOMAINS; do
+	#echo "$DOMAIN"
+	REDIRECTION=$(wget $DOMAIN 2>&1 | grep Location: | tail -n 1 | cut -d ":" -f3 | sed 's/\[following\]//g' | sed 's/\///g')
+	if [[ -z "$REDIRECTION" ]]; then
+		echo "$DOMAIN ==> KO" >> $OUTPUT_FILE
+	else
+		echo "$DOMAIN  ==> $REDIRECTION" >> $OUTPUT_FILE
+	fi
+done
+exit 0
